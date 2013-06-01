108f5665a52bf722f7eb3b44a6c713192b196ed4
vamshi <vrokandla@gmail.com>
2013-06-01 02:48:48 -0700

    initial commit

---
+++ child-two/src/main/java/com/cnmm2/TestClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.cnmm2;
+
+/**
+ * @author varokand
+ *
+ */
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
